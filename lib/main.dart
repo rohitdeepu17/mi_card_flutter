e745e913eb9e75347522c2bd584892fb7b7548da
Rohit Singla <rohitdeepu17@gmail.com>
2023-05-06 00:46:34 +0530

    add rounded image and styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MyApp()
-  );
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,29 +12,14 @@ class MyApp extends StatelessWidget{
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.end,
             children: <Widget>[
-              Container(
-                width: 300.0,
-                height: 100.0,
-                color: Colors.red,
-                child: Text('Container 1'),
+              CircleAvatar(
+                backgroundImage: AssetImage('images/modi.png'),
+                radius: 50.0,
               ),
-              SizedBox(
-                height: 40.0,
-              ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.blue,
-                child: Text('Container 2'),
-              ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.pink,
-                child: Text('Container 1'),
+              Text(
+                'Modi',
+                style: TextStyle(fontSize: 40.0, color: Colors.white),
               )
             ],
           ),
@@ -44,5 +27,4 @@ class MyApp extends StatelessWidget{
       ),
     );
   }
-
 }
