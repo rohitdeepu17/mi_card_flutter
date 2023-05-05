5faa9b99bfcef6f1a64010b0a092ac6ccb449d35
Rohit Singla <rohitdeepu17@gmail.com>
2023-05-05 23:24:44 +0530

    single child widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,28 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.all(20.0),
+            padding: EdgeInsets.all(20.0),
+            color: Colors.white,
+            child: Text('Hello'),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
+
 }
