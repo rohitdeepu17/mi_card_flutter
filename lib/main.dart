6fa7384d90faaf0aaaee044900f42aba753168c8
Rohit Singla <rohitdeepu17@gmail.com>
2023-05-06 02:05:46 +0530

    custom fonts using assets and icons

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,64 @@ class MyApp extends StatelessWidget {
               ),
               Text(
                 'Modi',
-                style: TextStyle(fontSize: 40.0, color: Colors.white),
+                style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontFamily: 'Freehand'),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.phone,
+                    color: Colors.teal,),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+917259890479',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.email,
+                      color: Colors.teal,),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'rohitdeepu17@gmail.com',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0
+                      ),
+                    )
+                  ],
+                ),
               )
             ],
           ),
